b765033e8e952850b89e925ab56f036679494321
YYUNUN <165138740+YYUNUN@users.noreply.github.com>
2024-06-10 01:05:28 +0800

    Update untitled.py

--- untitled.py
+++ untitled.py
@@ -31,90 +31,81 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     mtext = event.message.text
-    if mtext == '@傳送文字':
-        try:
-            message = TextSendMessage(
-                text = "我是 Linebot，\n您好！"
-            )
-            line_bot_api.reply_message(event.reply_token,message)
-        except:
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+    if mtext == '@按鈕樣板':
+        sendButton(event)
 
-    elif mtext == '@傳送圖片':
-        try:
-            message = ImageSendMessage(
-                original_content_url = "https://i.imgur.com/4QfKuz1.png",
-                preview_image_url = "https://i.imgur.com/4QfKuz1.png"
-            )
-            line_bot_api.reply_message(event.reply_token,message)
-        except:
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+    elif mtext == '@確認樣板':
+        sendConfirm(event)
+    
+    elif mtext == '@購買披薩':
+        sendPizza(event)
 
-    elif mtext == '@傳送貼圖':
-        try:
-            message = StickerSendMessage(  #貼圖兩個id需查表
-                package_id='1',
-                sticker_id='2'
-            )
-            line_bot_api.reply_message(event.reply_token, message)
-        except:
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+    elif mtext == '@yes':
+        sendYes(event)
 
-    elif mtext == '@多項傳送':
-        try:
-            message = [  #串列
-                StickerSendMessage(  #傳送貼圖
-                    package_id='1',
-                    sticker_id='2'
-                ),
-                TextSendMessage(  #傳送文字
-                    text = "這是 Pizza 圖片！"
-                ),
-                ImageSendMessage(  #傳送圖片
-                    original_content_url = "https://i.imgur.com/4QfKuz1.png",
-                    preview_image_url = "https://i.imgur.com/4QfKuz1.png"
-                )
-            ]
-            line_bot_api.reply_message(event.reply_token,message)
-        except:
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
-
-    elif mtext == '@傳送位置':
-        try:
-            message = LocationSendMessage(
-                title='101大樓',
-                address='台北市信義路五段7號',
-                latitude=25.034207,  #緯度
-                longitude=121.564590  #經度
+def sendButton(event):  #按鈕樣版
+    try:
+        message = TemplateSendMessage(
+            alt_text='按鈕樣板',
+            template=ButtonsTemplate(
+                thumbnail_image_url='https://i.imgur.com/4QfKuz1.png',  #顯示的圖片
+                title='按鈕樣版示範',  #主標題
+                text='請選擇：',  #副標題
+                actions=[
+                    MessageTemplateAction(  #顯示文字計息
+                        label='文字訊息',
+                        text='@購買披薩'
+                    ),
+                    URITemplateAction(  #開啟網頁
+                        label='連結網頁',
+                        uri='http://www.e-happy.com.tw'
+                    )
+                ]
             )
-            line_bot_api.reply_message(event.reply_token, message)
-        except:
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+        )
+        line_bot_api.reply_message(event.reply_token, message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
 
-    if mtext == '@快速選單':
-        try:
-            message = TextSendMessage(
-                text='請選擇最喜歡的程式語言',
-                quick_reply=QuickReply(
-                    items=[
-                        QuickReplyButton(
-                            action=MessageAction(label="Python", text="Python")
-                        ),
-                        QuickReplyButton(
-                            action=MessageAction(label="Java", text="Java")
-                        ),
-                        QuickReplyButton(
-                            action=MessageAction(label="C#", text="C#")
-                        ),
-                        QuickReplyButton(
-                            action=MessageAction(label="Basic", text="Basic")
-                        ),
-                    ]
-                )
+def sendConfirm(event):  #確認樣板
+    try:
+        message = TemplateSendMessage(
+            alt_text='確認樣板',
+            template=ConfirmTemplate(
+                text='你確定要購買這項商品嗎？',
+                actions=[
+                    MessageTemplateAction(  #按鈕選項
+                        label='是',
+                        text='@yes'
+                    ),
+                    MessageTemplateAction(
+                        label='否',
+                        text='@no'
+                    )
+                ]
             )
-            line_bot_api.reply_message(event.reply_token,message)
-        except:
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+        )
+        line_bot_api.reply_message(event.reply_token, message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+
+def sendPizza(event):
+    try:
+        message = TextSendMessage(
+            text = '感謝您購買披薩，我們將盡快為您製作。'
+        )
+        line_bot_api.reply_message(event.reply_token, message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+
+def sendYes(event):
+    try:
+        message = TextSendMessage(
+            text='感謝您的購買，\n我們將盡快寄出商品。',
+        )
+        line_bot_api.reply_message(event.reply_token, message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
 
 if __name__ == '__main__':
     app.run()
